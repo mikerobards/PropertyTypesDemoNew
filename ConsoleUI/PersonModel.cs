5007d034ee184eeb927ca3f9bd73fe445e22545f
Michael Robards <mikerobards@icloud.com>
2022-03-04 15:47:52 -0500

    Add project files.

---
+++ ConsoleUI/PersonModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class PersonModel
+    {
+        public string FirstName { private get; set; }
+        public string LastName { get; private set; }
+        //public int Age { get; set; }
+        private int _age;
+
+        public int Age
+        {
+            get 
+            { 
+                return _age; 
+            }
+            set 
+            {
+                if (value >= 0 && value < 126)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age needs to be in a valid range.");
+                }
+            }
+        }
+
+        //public string SSN { get; set; }
+        private string _ssn;
+
+        public string SSN
+        {
+            get 
+            {
+                // 123-45-6789
+                string output = "***-**-" + _ssn.Substring(_ssn.Length - 4);
+                return output; 
+            }
+            set { _ssn = value; }
+        }
+
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleUI;
+
+PersonModel person = new PersonModel();
+
+person.FirstName = "Mike";
+person.LastName = "Robards";
+person.Age = 50;
+person.SSN = "123-45-6789";
+
+Console.WriteLine(person.SSN);
